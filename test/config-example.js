3c319224e1cb1e90082a88dbb84beab4e9dd9644
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2018-08-17 01:02:53 +0200

    adding more examples

--- test/config-example.js
+++ test/config-example.js
@@ -8,10 +8,43 @@ module.exports = async () => {
       target: 'http://localhost:3000',
       middlewares: [],
       hooks: {
-        onRequest: async (req, reply) => {},
+        async onRequest (req, reply) {},
 
         // https://github.com/fastify/fastify-reply-from#replyfromsource-opts
-        onResponse: (res, reply) => reply.send(res) // NOTE: reply function is available here
+        onResponse (res, reply) { reply.send(res) } // NOTE: reply function is available here
+      }
+    }, {
+      prefix: '/users/proxy-aborted',
+      prefixRewrite: '',
+      target: 'http://localhost:5000',
+      middlewares: [],
+      hooks: {
+        async onRequest (req, reply) {
+          reply.code(200).send('Hello World!')
+          return true
+        }
+      }
+    }, {
+      prefix: '/users/response-time',
+      prefixRewrite: '',
+      target: 'http://localhost:3000',
+      middlewares: [require('response-time')()],
+      hooks: {
+        rewriteHeaders (headers) {
+          headers['post-processed'] = true
+
+          return headers
+        }
+      }
+    }, {
+      prefix: '/users/on-request-error',
+      prefixRewrite: '',
+      target: 'http://localhost:3000',
+      middlewares: [],
+      hooks: {
+        async onRequest (req, reply) {
+          throw new Error('ups, pre-processing error...')
+        }
       }
     }]
   }
--- test/feature.test.js
+++ test/feature.test.js
@@ -1,18 +1,33 @@
-/* global describe, it */
+/* global describe, it, setImmediate */
 const Fastify = require('fastify')
 const expect = require('chai').expect
 const request = require('supertest')
+const configValidate = require('./../src/config-validate')
 
 const fastify = Fastify({
   logger: false
 })
-fastify.register(require('fastify-reply-from'))
 
 let gateway, remote
 
 describe('API Gateway', () => {
+  it('config validate should fail', function (done) {
+    try {
+      configValidate({})
+    } catch (err) {
+      expect(err.message).to.equal('Gateway configuration is invalid!')
+      done()
+    }
+  })
+
+  it('config validate success', async () => {
+    const config = await require('./config-example')()
+    configValidate(config)
+  })
+
   it('initialize', async () => {
     // init gateway
+    fastify.register(require('fastify-reply-from'))
     fastify.register(require('./../index'),
       await require('./config-example')()
     )
@@ -26,12 +41,39 @@ describe('API Gateway', () => {
     await remote.start(3000)
   })
 
-  it('(cors) OPTIONS /users/info - 204', async () => {
+  it('(cors present) OPTIONS /users/response-time/info - 204', async () => {
+    await request(gateway)
+      .options('/users/response-time/info')
+      .expect(204)
+      .then((response) => {
+        expect(response.header['access-control-allow-origin']).to.equal('*')
+      })
+  })
+
+  it('(cors present) OPTIONS /users/info - 204', async () => {
     await request(gateway)
       .options('/users/info')
       .expect(204)
       .then((response) => {
         expect(response.header['access-control-allow-origin']).to.equal('*')
+      })
+  })
+
+  it('(response-time not present) OPTIONS /users/info - 204', async () => {
+    await request(gateway)
+      .options('/users/info')
+      .expect(204)
+      .then((response) => {
+        expect(response.header['x-response-time']).to.equal(undefined)
+      })
+  })
+
+  it('(response-time present) GET /users/response-time/info - 200', async () => {
+    await request(gateway)
+      .get('/users/response-time/info')
+      .expect(200)
+      .then((response) => {
+        expect(typeof response.header['x-response-time']).to.equal('string')
       })
   })
 
@@ -44,10 +86,37 @@ describe('API Gateway', () => {
       })
   })
 
+  it('GET /users/proxy-aborted/info - 200', async () => {
+    await request(gateway)
+      .get('/users/proxy-aborted/info')
+      .expect(200)
+      .then((response) => {
+        expect(response.text).to.equal('Hello World!')
+      })
+  })
+
   it('POST /users/info - 404', async () => {
     await request(gateway)
       .post('/users/find')
       .expect(404)
+  })
+
+  it('(hooks) GET /users/response-time/info - 200', async () => {
+    await request(gateway)
+      .get('/users/response-time/info')
+      .expect(200)
+      .then((response) => {
+        expect(response.header['post-processed']).to.equal('true')
+      })
+  })
+
+  it('(hooks) GET /users/on-request-error/info - 500', async () => {
+    await request(gateway)
+      .get('/users/on-request-error/info')
+      .expect(500)
+      .then((response) => {
+        expect(response.body.message).to.equal('ups, pre-processing error...')
+      })
   })
 
   it('close', async () => {
