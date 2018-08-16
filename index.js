aa1356f469c1a5508bfac7fb998621de106e7b3c
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2018-08-17 01:01:40 +0200

    adding reply paremeter to onRequest cb call

--- index.js
+++ index.js
@@ -16,14 +16,13 @@ function gateway (fastify, opts, next) {
     })
 
     // populating required NOOPS
-    route.hooks = route.hooks || {}
     route.hooks.onRequest = route.hooks.onRequest || (async (req, reply) => { })
     route.hooks.onResponse = route.hooks.onResponse || ((res, reply) => reply.send(res))
 
     // registering route handler
     fastify.all(route.prefix + '/*', (request, reply) => {
       request.req.url = request.req.url.replace(route.prefix, route.prefixRewrite)
-      route.hooks.onRequest(request).then(shouldAbortProxy => {
+      route.hooks.onRequest(request, reply).then(shouldAbortProxy => {
         // check if request proxy to remote should be aborted
         if (!shouldAbortProxy) {
           reply.from(route.target + request.req.url, Object.assign({}, route.hooks, {
