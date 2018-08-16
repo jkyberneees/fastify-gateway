9f8a678b9a6bfa501f053c19e58ab60ebca3269a
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2018-08-16 23:18:54 +0200

    creating plugin

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 const configValidate = require('./src/config-validate')
+const fp = require('fastify-plugin')
 
-module.exports = (fastify, config) => {
-  configValidate(config)
+function gateway (fastify, opts, next) {
+  configValidate(opts)
 
-  return {
-    register: () => {
-      // registering global middlewares
-      config.middlewares.forEach(middleware => {
-        fastify.use(middleware)
+  // registering global middlewares
+  opts.middlewares.forEach(middleware => {
+    fastify.use(middleware)
+  })
+
+  opts.routes.forEach(route => {
+    // registering routes level middlewares
+    route.middlewares.forEach(middleware => {
+      fastify.use(route.prefix, middleware)
+    })
+
+    // populating required NOOPS
+    route.hooks = route.hooks || {}
+    route.hooks.onRequest = route.hooks.onRequest || (async (req, reply) => { })
+    route.hooks.onResponse = route.hooks.onResponse || ((res, reply) => reply.send(res))
+
+    // registering route handler
+    fastify.all(route.prefix + '/*', (request, reply) => {
+      request.req.url = request.req.url.replace(route.prefix, route.prefixRewrite)
+      route.hooks.onRequest(request).then(shouldAbortProxy => {
+        // check if request proxy to remote should be aborted
+        if (!shouldAbortProxy) {
+          reply.from(route.target + request.req.url, Object.assign({}, route.hooks, {
+            // override onResponse hook to pass the "reply" object
+            onResponse: (res) => route.hooks.onResponse(res, reply)
+          }))
+        }
+      }).catch(err => {
+        reply.send(err)
       })
+    })
+  })
 
-      config.routes.forEach(route => {
-        // registering routes level middlewares
-        route.middlewares.forEach(middleware => {
-          fastify.use(route.prefix, middleware)
-        })
-
-        // populating required NOOPS
-        route.hooks = route.hooks || {}
-        route.hooks.onRequest = route.hooks.onRequest || (async (req, reply) => { })
-        route.hooks.onResponse = route.hooks.onResponse || ((res, reply) => reply.send(res))
-
-        // registering route handler
-        fastify.all(route.prefix + '/*', (request, reply) => {
-          request.req.url = request.req.url.replace(route.prefix, route.prefixRewrite)
-          route.hooks.onRequest(request).then(shouldAbortProxy => {
-            // check if request proxy to remote should be aborted
-            if (!shouldAbortProxy) {
-              reply.from(route.target + request.req.url, Object.assign({}, route.hooks, {
-                // override onResponse hook to pass the "reply" object
-                onResponse: (res) => route.hooks.onResponse(res, reply)
-              }))
-            }
-          }).catch(err => {
-            reply.send(err)
-          })
-        })
-      })
-    }
-  }
+  next()
 }
+
+module.exports = fp(gateway)
--- test/feature.test.js
+++ test/feature.test.js
@@ -13,29 +13,41 @@ let gateway, remote
 describe('API Gateway', () => {
   it('initialize', async () => {
     // init gateway
-    require('./../index')(
-      fastify,
-      await require('./config-example')(fastify)
-    ).register()
+    fastify.register(require('./../index'),
+      await require('./config-example')()
+    )
     gateway = await fastify.listen(8080)
 
-    // init remote
+    // init remote service
     remote = require('restana')({})
-    remote.get('/info', (req, res) => {
-      res.send({
-        name: 'fastify-gateway'
-      })
-    })
-    await remote.start()
+    remote.get('/info', (req, res) => res.send({
+      name: 'fastify-gateway'
+    }))
+    await remote.start(3000)
   })
 
-  it('GET /users', async () => {
+  it('(cors) OPTIONS /users/info - 204', async () => {
+    await request(gateway)
+      .options('/users/info')
+      .expect(204)
+      .then((response) => {
+        expect(response.header['access-control-allow-origin']).to.equal('*')
+      })
+  })
+
+  it('GET /users/info - 200', async () => {
     await request(gateway)
       .get('/users/info')
       .expect(200)
       .then((response) => {
         expect(response.body.name).to.equal('fastify-gateway')
       })
+  })
+
+  it('POST /users/info - 404', async () => {
+    await request(gateway)
+      .post('/users/find')
+      .expect(404)
   })
 
   it('close', async () => {
