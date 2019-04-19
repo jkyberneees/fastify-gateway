af7a2abd22432d29f19c1c6e0fbcff8c4609c775
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-04-19 08:28:12 +0200

    removing boilerplate

--- benchmark/gateway.js
+++ benchmark/gateway.js
@@ -2,18 +2,9 @@ const fastify = require('fastify')({})
 fastify.register(require('fastify-reply-from'))
 fastify.register(require('..'), {
 
-  middlewares: [
-  ],
-
   routes: [{
     prefix: '/service',
-    prefixRewrite: '',
-    target: 'http://localhost:3000',
-    middlewares: [],
-    hooks: {
-      // async onRequest (req, reply) {},
-      // onResponse (res, reply) { reply.send(res) }
-    }
+    target: 'http://localhost:3000'
   }]
 })
 
