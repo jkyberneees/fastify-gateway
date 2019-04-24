cebc7d2ed1314a07baaade0fe1806649a5556957
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-04-24 09:37:18 +0200

    fixing double stream reading issue on caching plugin

--- src/plugins/cache.js
+++ src/plugins/cache.js
@@ -1,16 +1,12 @@
 const fp = require('fastify-plugin')
 const CacheManager = require('cache-manager')
 const ms = require('ms')
-const rparser = require('./../res-parser')
+const onFinished = require('./on-finished')
 const matcher = require('matcher')
 
 const X_CACHE_EXPIRE = 'x-cache-expire'
 const X_CACHE_TIMEOUT = 'x-cache-timeout'
 const X_CACHE_HIT = 'x-cache-hit'
-
-const {
-  kReplyHeaders: FASTIFY_HEADERS
-} = require('fastify/lib/symbols')
 
 const plugin = (fastify, opts, next) => {
   opts = Object.assign({
@@ -19,6 +15,34 @@ const plugin = (fastify, opts, next) => {
 
   // creating multi-cache instance
   const multiCache = CacheManager.multiCaching(opts.stores)
+
+  fastify.addHook('onRequest', (request, reply, next) => {
+    const { res } = reply
+    const { req } = request
+
+    onFinished(res, (payload) => {
+      // avoid double caching
+      if (req.cacheHit) return
+
+      if (payload.headers[X_CACHE_EXPIRE]) {
+        // support service level expiration
+        const keysPattern = payload.headers[X_CACHE_EXPIRE]
+        // delete keys on all cache tiers
+        opts.stores.forEach(cache => getKeys(cache, keysPattern).then(keys => {
+          multiCache.del(keys)
+        }))
+      } else if (payload.headers[X_CACHE_TIMEOUT]) {
+        // we need to cache response
+        multiCache.set(req.cacheKey, JSON.stringify(payload), {
+          // @NOTE: cache-manager uses seconds as TTL unit
+          // restrict to min value "1 second"
+          ttl: Math.max(ms(payload.headers[X_CACHE_TIMEOUT]), 1000) / 1000
+        })
+      }
+    })
+
+    return next()
+  })
 
   fastify.addHook('preHandler', async (request, reply, next) => {
     const { req } = request
@@ -34,7 +58,10 @@ const plugin = (fastify, opts, next) => {
     if (!cached) return next()
 
     // respond from cache if there is a hit
-    const { headers, payload } = JSON.parse(cached)
+    let { status, headers, data } = JSON.parse(cached)
+    if (typeof data === 'object' && data.type === 'Buffer') {
+      data = Buffer.from(data.data)
+    }
     headers[X_CACHE_HIT] = '1'
 
     // set cached response headers
@@ -42,38 +69,10 @@ const plugin = (fastify, opts, next) => {
 
     // send cached payload
     req.cacheHit = true
-    reply.send(await rparser.out(payload))
+    reply.code(status).send(data)
   })
 
-  fastify.addHook('onSend', (request, reply, payload, next) => {
-    const { req } = request
-
-    // avoid double caching
-    if (req.cacheHit) return next()
-
-    if (reply.hasHeader(X_CACHE_EXPIRE)) {
-      // support service level expiration
-      const keysPattern = reply.getHeader(X_CACHE_EXPIRE)
-      // delete keys on all cache tiers
-      opts.stores.forEach(cache => getKeys(cache, keysPattern).then(keys => multiCache.del(keys)))
-    } else if (reply.hasHeader(X_CACHE_TIMEOUT)) {
-      // we need to cache response
-      rparser.in(payload).then(payload => {
-        multiCache.set(req.cacheKey, JSON.stringify({
-          headers: reply[FASTIFY_HEADERS],
-          payload
-        }), {
-          // @NOTE: cache-manager uses seconds as TTL unit
-          // restrict to min value "1 second"
-          ttl: Math.max(ms(reply.getHeader(X_CACHE_TIMEOUT)), 1000) / 1000
-        })
-      })
-    }
-
-    return next()
-  })
-
-  next()
+  return next()
 }
 
 const get = (cache, key) => new Promise((resolve) => {
---
+++ src/plugins/on-finished.js
@@ -0,0 +1,58 @@
+// inspired on https://github.com/kwhitley/apicache/blob/master/src/apicache.js
+
+module.exports = (res, cb) => {
+  res._parent = {
+    write: res.write,
+    writeHead: res.writeHead,
+    end: res.end,
+    content: undefined
+  }
+
+  res.write = function (content) {
+    accumulate(res, content)
+    return res._parent.write.apply(res, arguments)
+  }
+
+  res.end = function (content, encoding) {
+    accumulate(res, content)
+    const headers = res._headers
+    const payload = map(res.statusCode, headers, res._parent.content, encoding)
+
+    setImmediate(() => {
+      cb(payload)
+    })
+
+    return res._parent.end.apply(res, arguments)
+  }
+}
+
+function map (status, headers, data, encoding) {
+  return {
+    status: status,
+    headers: headers,
+    data: data,
+    encoding: encoding
+  }
+}
+
+function accumulate (res, content) {
+  if (content) {
+    if (typeof (content) === 'string') {
+      res._parent.content = (res._parent.content || '') + content
+    } else if (Buffer.isBuffer(content)) {
+      let oldContent = res._parent.content
+
+      if (typeof oldContent === 'string') {
+        oldContent = Buffer.from(oldContent)
+      }
+
+      if (!oldContent) {
+        oldContent = Buffer.alloc(0)
+      }
+
+      res._parent.content = Buffer.concat([oldContent, content], oldContent.length + content.length)
+    } else {
+      res._parent.content = content
+    }
+  }
+}
